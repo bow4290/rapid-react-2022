9faf12665060d781d3dc73c3d7505da11ccae711
Gabrielle Jones <gabbijones2006@gmail.com>
2022-01-15 11:26:55 -0500

    Intermediate drive train.

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -1,8 +1,14 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX; // Falcon500 onboard speed controller
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
@@ -16,6 +22,14 @@ public class DrivetrainSubsystem extends SubsystemBase {
   private WPI_TalonFX rightMotor2 = new WPI_TalonFX(DriveConstants.rightMotor2Channel);
 
   private final DifferentialDrive drivetrain;
+
+  private final DoubleSolenoid gearShiftSolenoid;
+
+  public enum GearShiftStatus{
+    UP, DOWN
+  }
+
+  public static GearShiftStatus gearShiftStatus;
 
   public DrivetrainSubsystem() {
     leftMotor1.setInverted(TalonFXInvertType.Clockwise);
@@ -36,11 +50,60 @@ public class DrivetrainSubsystem extends SubsystemBase {
     rightMotor1.configOpenloopRamp(0.5);
     rightMotor2.configOpenloopRamp(0.5);
 
+    leftMotor1.configVoltageCompSaturation(11);
+    leftMotor2.configVoltageCompSaturation(11);
+    rightMotor1.configVoltageCompSaturation(11);
+    rightMotor2.configVoltageCompSaturation(11);
+
+    leftMotor1.enableVoltageCompensation(true); 
+    leftMotor2.enableVoltageCompensation(true); 
+    rightMotor1.enableVoltageCompensation(true); 
+    rightMotor2.enableVoltageCompensation(true);
+
+    leftMotor1.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
+    rightMotor1.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
+
     drivetrain = new DifferentialDrive(leftMotor1, rightMotor1);
+
+    gearShiftSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, DriveConstants.gearShiftUpChannel, DriveConstants.gearShiftDownChannel);
   }
 
-  public void drive(double leftSpeed, double rightSpeed) {
+  public GearShiftStatus getSolenoid(){
+    return gearShiftStatus;
+  }
+
+  public void drive(double leftSpeed, double rightSpeed) { 
     drivetrain.tankDrive(leftSpeed, rightSpeed, true);
   }
 
+  public void resetDriveEncoders() {
+    leftMotor1.setSelectedSensorPosition(0);
+    rightMotor1.setSelectedSensorPosition(0);
+  }
+
+  // add distance conversion for shifting
+  public double getLeftCalculatedPosition() {
+    return (getLeftRawEncoderPosition() * (DriveConstants.encoderDistanceConversion));
+  }
+
+  public double getRightCalculatedPosition() {
+    return (getRightRawEncoderPosition() * (DriveConstants.encoderDistanceConversion));
+  }
+
+  private double getLeftRawEncoderPosition() {
+    return leftMotor1.getSelectedSensorPosition();
+  }
+
+  private double getRightRawEncoderPosition() {
+    return rightMotor1.getSelectedSensorPosition();
+  }
+
+
+  @Override
+  public void periodic(){
+    SmartDashboard.putNumber("Left Drive Position", getLeftCalculatedPosition());
+    SmartDashboard.putNumber("Right Drive Position", getRightCalculatedPosition());
+    SmartDashboard.putNumber("Left Raw Drive Position", getLeftRawEncoderPosition());
+    SmartDashboard.putNumber("Right Raw Drive Position", getRightRawEncoderPosition());
+  } 
 }
