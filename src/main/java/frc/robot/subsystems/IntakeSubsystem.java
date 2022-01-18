d262f91386da88d523ea5a2e925f0b29f85f8385
Apppppppple <19359968+Apppppppple@users.noreply.github.com>
2022-01-18 18:38:21 -0500

    Intake subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,17 +1,53 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
-  /** Creates a new IntakeSubsystem. */
-  public IntakeSubsystem() {}
+  private final WPI_VictorSPX intakeMotor;
+  private final DoubleSolenoid intakeSolenoid;
+
+  public enum IntakeStatus{
+    DOWN, UP
+  }
+  public static IntakeStatus intakeStatus;
+
+  public IntakeSubsystem() {
+    intakeMotor = new WPI_VictorSPX(IntakeConstants.intakeMotorChannel);
+    intakeSolenoid = new DoubleSolenoid(IntakeConstants.intakeUpChannel, IntakeConstants.intakeDownChannel);
+  
+    intakeMotor.setInverted(false);
+    intakeStatus = IntakeStatus.UP;
+  }
+
+  public void retractIntake(){
+    intakeSolenoid.set(DoubleSolenoid.Value.kForward);
+    intakeStatus = IntakeStatus.UP;
+  }
+
+  public void extendIntake(){
+    intakeSolenoid.set(DoubleSolenoid.Value.kReverse);
+    intakeStatus = IntakeStatus.DOWN;
+  }
+
+  public void intakeIn(double intakeSpeed){
+    intakeMotor.set(ControlMode.PercentOutput, intakeSpeed);
+  }
+
+  public void intakeStop(){
+    intakeMotor.set(ControlMode.PercentOutput, 0);
+  }
+
+  public static IntakeStatus getIntakePosition(){
+    return intakeStatus;
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    // TODO: smartboard
   }
 }
