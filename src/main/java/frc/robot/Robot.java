b8b8a45cc4fd8a5e1f84dd1825e999f6c112875a
Austin Fifield <afifield@uncc.edu>
2022-01-20 21:02:30 -0500

    Multi-speed button control for Falcon 500 base class.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,103 +7,93 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.*;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  TalonFX myTalon = new TalonFX(DriveConstants.myFalconChannel);
-  private Command m_autonomousCommand;
+  Joystick xboxController = new Joystick(0);
+  JoystickButton xboxAButton = new JoystickButton(xboxController, 1);
+  JoystickButton xboxBButton = new JoystickButton(xboxController, 2);
+  JoystickButton xboxXButton = new JoystickButton(xboxController, 3);
+  JoystickButton xboxYButton = new JoystickButton(xboxController, 4);
 
+  TalonFX myTalon = new TalonFX(DriveConstants.myFalconChannel);
+
+  private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
     myTalon.set(ControlMode.PercentOutput, 0.0);
     myTalon.setInverted(false);
     myTalon.setSensorPhase(false);
+    myTalon.configVoltageCompSaturation(11);
+    myTalon.enableVoltageCompensation(true);
+    
     m_robotContainer = new RobotContainer();
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
-    SmartDashboard.putNumber("Talon Velocity: ", myTalon.getSelectedSensorVelocity());
-    SmartDashboard.putNumber("Talon Position: ", myTalon.getSelectedSensorPosition());
-    SmartDashboard.putNumber("Talon Current: ", myTalon.getSupplyCurrent());
+
+    SmartDashboard.putNumber("Talon Velocity (RPM): ", myTalon.getSelectedSensorVelocity()*600/2048);
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {}
 
   @Override
   public void disabledPeriodic() {}
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-    // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
   }
 
-  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    myTalon.set(ControlMode.PercentOutput, 0.3);
+
+    double motorPercentOutput = 0;
+
+    if(xboxAButton.get()) {
+      motorPercentOutput = 1.0;
+    } else if(xboxBButton.get()) {
+      motorPercentOutput = 0.75;
+    } else if(xboxXButton.get()) {
+      motorPercentOutput = 0.5;
+    } else if(xboxYButton.get()) {
+      motorPercentOutput = 0.25;
+    }
+
+    myTalon.set(ControlMode.PercentOutput, motorPercentOutput);
+
   }
 
   @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 }
