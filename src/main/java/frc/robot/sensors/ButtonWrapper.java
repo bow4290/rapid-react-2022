5a344b4634f56c01a71027422f6a03e26362b2b6
Austin <afifield@uncc.edu>
2022-01-22 11:21:55 -0500

    Preliminary Button class

---
+++ src/main/java/frc/robot/sensors/ButtonWrapper.java
@@ -0,0 +1,23 @@
+
+package frc.robot.sensors;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class ButtonWrapper {
+
+    private int buttonPort;
+    private enum ButtonType{ NO, NC };
+    private ButtonType buttonType;
+
+    private final DigitalInput button = new DigitalInput(buttonPort);
+    
+    public ButtonWrapper(ButtonType buttonType, int buttonPort) {
+        this.buttonType = buttonType;
+        this.buttonPort = buttonPort;
+    }
+
+    private boolean isPressed() { return (buttonType == ButtonType.NO) ? button.get() : !button.get(); }
+
+    private boolean isNotPressed() { return (buttonType == ButtonType.NO) ? !button.get() : button.get(); }
+
+}
