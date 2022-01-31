cafa7e7b987c8aee4927fc1899b05fa2044be2c7
Austin Fifield <afifield@uncc.edu>
2022-01-31 18:04:10 -0500

    Removed default comments in preparation for climber updates.

--- src/main/java/frc/robot/subsystems/ClimberFrontSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberFrontSubsystem.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
@@ -13,7 +9,6 @@ import frc.robot.Constants.DriveConstants;
 public class ClimberFrontSubsystem extends SubsystemBase {
   private CANSparkMax frontClimberMotorChannel = new CANSparkMax(DriveConstants.frontClimberMotorChannel, MotorType.kBrushless);
   
-  /** Creates a new ClimberFrontSubsystem. */
   public ClimberFrontSubsystem() { frontClimberMotorChannel.setInverted(false); }
 
   public void extendFrontClimber(double climberSpeed) { frontClimberMotorChannel.set(climberSpeed); }
@@ -24,6 +19,5 @@ public class ClimberFrontSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 }
--- src/main/java/frc/robot/subsystems/ClimberRearSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberRearSubsystem.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
@@ -13,7 +9,6 @@ import frc.robot.Constants.DriveConstants;
 public class ClimberRearSubsystem extends SubsystemBase {
   private CANSparkMax rearClimberMotorChannel = new CANSparkMax(DriveConstants.rearClimberMotorChannel, MotorType.kBrushless);
   
-  /** Creates a new ClimberRearSubsystem. */
   public ClimberRearSubsystem() { rearClimberMotorChannel.setInverted(false); }
 
   public void extendRearClimber(double climberSpeed) { rearClimberMotorChannel.set(climberSpeed); }
@@ -24,6 +19,5 @@ public class ClimberRearSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 }
