84c0ff5cb4def03f68e1b277d1fbafa936db02bf
Austin Fifield <afifield@uncc.edu>
2022-02-10 20:20:34 -0500

    Updated CAN channels.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,13 +2,13 @@ package frc.robot;
 
 public final class Constants {
   public static final class DriveConstants {
-    public static int leftMotor1Channel = 0;
-    public static int leftMotor2Channel = 1;
-    public static int rightMotor1Channel = 2;
-    public static int rightMotor2Channel = 3;
+    public static int leftMotor1Channel = 7;
+    public static int leftMotor2Channel = 8;
+    public static int rightMotor1Channel = 5;
+    public static int rightMotor2Channel = 6;
 
-    public static int gearShiftUpChannel = 6;
-    public static int gearShiftDownChannel = 9;
+    public static int gearShiftUpChannel = 0;
+    public static int gearShiftDownChannel = 1;
 
     public static double encoderDistanceConversion = 1.0;
   }
@@ -21,8 +21,8 @@ public final class Constants {
 
   public static final class IntakeConstants {
     public static int intakeMotorChannel = 0;
-    public static int intakeUpChannel = 0;
-    public static int intakeDownChannel = 0;
+    public static int intakeUpChannel = 6;
+    public static int intakeDownChannel = 7;
   }
 
   public static final class LimelightConstants {
