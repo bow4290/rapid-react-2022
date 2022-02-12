1f0087db938271b090d0c34b5034debcbdd015e0
Austin Fifield <afifield@uncc.edu>
2022-02-12 14:48:34 -0500

    Stripped of non hood code.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,28 +1,8 @@
 package frc.robot;
 
 public final class Constants {
-  public static final class DriveConstants {
-    public static int leftMotor1Channel = 7;
-    public static int leftMotor2Channel = 8;
-    public static int rightMotor1Channel = 5;
-    public static int rightMotor2Channel = 6;
-
-    public static int gearShiftUpChannel = 0;
-    public static int gearShiftDownChannel = 1;
-
-    public static double encoderDistanceConversion = 1.0;
-  }
-
   public static final class JoystickConstants {
-    public static int LEFT_JOYSTICK = 0;
-    public static int RIGHT_JOYSTICK = 1;
     public static int XBOX_CONTROLLER = 2;
-  }
-
-  public static final class IntakeConstants {
-    public static int intakeMotorChannel = 0;
-    public static int intakeUpChannel = 0;
-    public static int intakeDownChannel = 0;
   }
 
   public static final class HoodConstants {
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,21 +2,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.Constants.JoystickConstants;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.sensors.BallIdentification;
 import frc.robot.sensors.Limelight;
 import frc.robot.sensors.RevColorSensor;
-import frc.robot.commands.Drivetrain.*;
 import frc.robot.commands.Hood.DefaultHoodCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.*;
 
 public class RobotContainer {
-  public static Joystick joystickLeft;
-  public static Joystick joystickRight;
   public static Joystick xboxController;
 
   public RevColorSensor redBallColorSensor;
@@ -26,37 +19,20 @@ public class RobotContainer {
 
   public Limelight limelight = new Limelight();
 
-  private DrivetrainSubsystem drivetrainSubsystem;
   private HoodSubsystem hoodSubsystem = new HoodSubsystem();
 
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
   public RobotContainer() {
-    joystickLeft = new Joystick(JoystickConstants.LEFT_JOYSTICK);
-    joystickRight = new Joystick(JoystickConstants.RIGHT_JOYSTICK);
     xboxController = new Joystick(JoystickConstants.XBOX_CONTROLLER);
 
     redBallColorSensor = new RevColorSensor(80, 180, 50, 80, 15, 40, 0, 2048);
     blueBallColorSensor = new RevColorSensor(10, 70, 50, 100, 40, 100, 0, 2048);
     
     ball = new BallIdentification(redBallColorSensor, blueBallColorSensor);
-
-    drivetrainSubsystem = new DrivetrainSubsystem();
-
-    drivetrainSubsystem.setDefaultCommand(new Drive(getLeftY(), getRightY(), drivetrainSubsystem));
+  
     hoodSubsystem.setDefaultCommand(new DefaultHoodCommand(limelight, hoodSubsystem));
 
     configureButtonBindings();
   }
-
-  public double getLeftY() { return joystickLeft.getY(); }
-
-  public double getLeftX() { return joystickLeft.getX(); }
-
-  public double getRightY() { return joystickRight.getY(); }
-
-  public double getRightX() { return joystickRight.getX(); }
 
   /* Xbox Controller Button Binding:
     Buttons:
@@ -72,34 +48,7 @@ public class RobotContainer {
       2 - LeftTrig    5 - RightY
   */
   private void configureButtonBindings() {
-    setJoystickButtonWhenPressed(joystickLeft, 1, new ShiftGearDown(drivetrainSubsystem));
-    setJoystickButtonWhenPressed(joystickRight, 1, new ShiftGearUp(drivetrainSubsystem));
   }
 
-  public Command getAutonomousCommand() { return m_autoCommand; }
-
-  /** WhenPressed runs the command once at the moment the button is pressed. */
-  private void setJoystickButtonWhenPressed(Joystick joystick, int button, CommandBase command) {
-    new JoystickButton(joystick, button).whenPressed(command);
-  }
-
-  /**
-   * WhileHeld constantly starts the command and repeatedly schedules while the
-   * button is held. Cancels when button is released.
-   */
-  private void setJoystickButtonWhileHeld(Joystick joystick, int button, CommandBase command) {
-    new JoystickButton(joystick, button).whileHeld(command);
-  }
-
-  /**
-   * WhenHeld starts the command once when the button is first pressed. Command
-   * runs until button is released or command interrupted.
-   */
-  private void setJoystickButtonWhenHeld(Joystick joystick, int button, CommandBase command) {
-    new JoystickButton(joystick, button).whenHeld(command);
-  }
-
-  private void setJoystickButtonToggleWhenPressed(Joystick joystick, int button, CommandBase command) {
-    new JoystickButton(joystick, button).toggleWhenPressed(command);
-  }
+  public Command getAutonomousCommand() { return null; }
 }
