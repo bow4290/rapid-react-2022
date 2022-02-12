cd342765a4cfbda1de34163ed15b6857d8ad56c3
Apppppppple <19359968+Apppppppple@users.noreply.github.com>
2022-02-12 14:43:07 -0500

    Change constants from static to static final
    This makes the constant immutable (i.e. actually constant), and enables
    java to optimize the code (e.g. inlining).

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,40 +2,40 @@ package frc.robot;
 
 public final class Constants {
   public static final class DriveConstants {
-    public static int leftMotor1Channel = 7;
-    public static int leftMotor2Channel = 8;
-    public static int rightMotor1Channel = 5;
-    public static int rightMotor2Channel = 6;
+    public static final int leftMotor1Channel = 7;
+    public static final int leftMotor2Channel = 8;
+    public static final int rightMotor1Channel = 5;
+    public static final int rightMotor2Channel = 6;
 
-    public static int gearShiftUpChannel = 0;
-    public static int gearShiftDownChannel = 1;
+    public static final int gearShiftUpChannel = 0;
+    public static final int gearShiftDownChannel = 1;
 
-    public static double targetLinearDistance = 60;
-    private static double stage1Ratio = 60/12;
-    private static double stage2Ratio = 28/28;
-    private static double lowSpreadRatio = 44/22;
-    private static double highSpreadRatio = 34/32;
-    private static double wheelDiameter = 3.75;
-    private static double wheelCircum = wheelDiameter*Math.PI;
-    private static int encoderCPR = 2048;
-    private static double lowCountsPerWheelRev = stage1Ratio * stage2Ratio * lowSpreadRatio * encoderCPR;
-    private static double highCountsPerWheelRev = stage1Ratio * stage2Ratio * highSpreadRatio * encoderCPR;
-    private static double lowGearTargetCounts = lowCountsPerWheelRev * targetLinearDistance * wheelCircum;
-    private static double highGearTargetCounts = highCountsPerWheelRev * targetLinearDistance * wheelCircum;
-    public static double encoderLowDistanceConversion = wheelCircum/(stage1Ratio * stage2Ratio * lowSpreadRatio * encoderCPR);
-    public static double encoderHighDistanceConversion = wheelCircum/(stage1Ratio * stage2Ratio * highSpreadRatio * encoderCPR);
+    public static final double targetLinearDistance = 60;
+    private static final double stage1Ratio = 60/12;
+    private static final double stage2Ratio = 28/28;
+    private static final double lowSpreadRatio = 44/22;
+    private static final double highSpreadRatio = 34/32;
+    private static final double wheelDiameter = 3.75;
+    private static final double wheelCircum = wheelDiameter*Math.PI;
+    private static final int encoderCPR = 2048;
+    private static final double lowCountsPerWheelRev = stage1Ratio * stage2Ratio * lowSpreadRatio * encoderCPR;
+    private static final double highCountsPerWheelRev = stage1Ratio * stage2Ratio * highSpreadRatio * encoderCPR;
+    private static final double lowGearTargetCounts = lowCountsPerWheelRev * targetLinearDistance * wheelCircum;
+    private static final double highGearTargetCounts = highCountsPerWheelRev * targetLinearDistance * wheelCircum;
+    public static final double encoderLowDistanceConversion = wheelCircum/(stage1Ratio * stage2Ratio * lowSpreadRatio * encoderCPR);
+    public static final double encoderHighDistanceConversion = wheelCircum/(stage1Ratio * stage2Ratio * highSpreadRatio * encoderCPR);
   }
 
   public static final class JoystickConstants {
-    public static int LEFT_JOYSTICK = 0;
-    public static int RIGHT_JOYSTICK = 1;
-    public static int XBOX_CONTROLLER = 2;
+    public static final int LEFT_JOYSTICK = 0;
+    public static final int RIGHT_JOYSTICK = 1;
+    public static final int XBOX_CONTROLLER = 2;
   }
 
   public static final class IntakeConstants {
-    public static int intakeMotorChannel = 44;
-    public static int intakeUpChannel = 6;
-    public static int intakeDownChannel = 7;
+    public static final int intakeMotorChannel = 44;
+    public static final int intakeUpChannel = 6;
+    public static final int intakeDownChannel = 7;
   }
 
   public static final class LimelightConstants {
