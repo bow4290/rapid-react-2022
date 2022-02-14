023f154e4d9b2070677eb81ddbd3aa91fea5851f
Apppppppple <19359968+Apppppppple@users.noreply.github.com>
2022-02-14 11:10:57 -0500

    Make resetting drivetrain encoders respect flags

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import frc.robot.Constants.Flags;
+
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
@@ -46,7 +48,7 @@ public class Robot extends TimedRobot {
       m_autonomousCommand.cancel();
     }
 
-    m_robotContainer.drivetrainSubsystem.resetDriveEncoders();
+    if (Flags.drivetrain) m_robotContainer.drivetrainSubsystem.resetDriveEncoders();
   }
 
   @Override
