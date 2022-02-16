c7aeba870968fa2ea2689eb7511855056fda5b4f
AhmadK <mxwu78@gmail.com>
2022-02-15 17:31:21 -0800

    Added "FrontClimberStatus" and ClimberFront data to GUI
    added "FrontClimberStatus" and whether ClimberFront is "EXTENDED" or "RETRACTED" on GUI

--- src/main/java/frc/robot/subsystems/ClimberFrontSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberFrontSubsystem.java
@@ -5,19 +5,30 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ClimberFrontSubsystem extends SubsystemBase {
   private CANSparkMax frontClimberMotorChannel = new CANSparkMax(ClimberConstants.frontClimberMotorChannel, MotorType.kBrushless);
-  
+  public enum FrontClimberStatus { EXTENDED, RETRACTED }
+  public static FrontClimberStatus frontClimberStatus = FrontClimberStatus.RETRACTED;
+
   public ClimberFrontSubsystem() { frontClimberMotorChannel.setInverted(false); }
 
-  public void extendFrontClimber(double climberSpeed) { frontClimberMotorChannel.set(climberSpeed); }
+  public void extendFrontClimber(double climberSpeed) { 
+    frontClimberMotorChannel.set(climberSpeed);
+    frontClimberStatus = FrontClimberStatus.EXTENDED;
+  }
 
-  public void retractFrontClimber(double climberSpeed) { frontClimberMotorChannel.set(-climberSpeed); }
+  public void retractFrontClimber(double climberSpeed) { frontClimberMotorChannel.set(-climberSpeed);
+    frontClimberStatus = FrontClimberStatus.RETRACTED;
+  }
 
   public void stopFrontClimber() { frontClimberMotorChannel.set(0); }
 
+  public static FrontClimberStatus getFrontClimberStatus() { return frontClimberStatus; }
+
   @Override
   public void periodic() {
+    SmartDashboard.putString("Front Climber Status", frontClimberStatus  == FrontClimberStatus.RETRACTED ? "RETRACTED"  : "EXTRENDED");
   }
 }
