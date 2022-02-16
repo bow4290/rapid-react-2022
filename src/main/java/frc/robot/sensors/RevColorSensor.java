059e0547933b8504d58f17b4c2d4d70df5f11ad8
Apppppppple <19359968+Apppppppple@users.noreply.github.com>
2022-02-15 19:26:19 -0500

    Add shuffleboard display for color sensors

--- src/main/java/frc/robot/sensors/RevColorSensor.java
+++ src/main/java/frc/robot/sensors/RevColorSensor.java
@@ -5,7 +5,11 @@
 
 package frc.robot.sensors;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
 import com.revrobotics.ColorSensorV3;
 
 public class RevColorSensor {
@@ -21,6 +25,20 @@ public class RevColorSensor {
   private int proxLowThresh;
   private int proxHighThresh;
 
+  private static int idGen = 0;
+  private int id;
+
+  private ShuffleboardTab tab;
+
+  private NetworkTableEntry redLowThreshShuffle;
+  private NetworkTableEntry redHighThreshShuffle;
+  private NetworkTableEntry greenLowThreshShuffle;
+  private NetworkTableEntry greenHighThreshShuffle;
+  private NetworkTableEntry blueLowThreshShuffle;
+  private NetworkTableEntry blueHighThreshShuffle;
+  private NetworkTableEntry proxLowThreshShuffle;
+  private NetworkTableEntry proxHighThreshShuffle;
+
   public RevColorSensor(int redLowThresh, int redHighThresh, int greenLowThresh,
                         int greenHighThresh, int blueLowThresh, int blueHighThresh,
                         int proxLowThresh, int proxHighThresh) {
@@ -32,7 +50,27 @@ public class RevColorSensor {
     this.blueHighThresh = blueHighThresh;
     this.proxLowThresh = proxLowThresh;
     this.proxHighThresh = proxHighThresh;
+
+    id = ++idGen;
+    tab = Shuffleboard.getTab("Color Sensor " + id);
+
+    redLowThreshShuffle   = tab.add("Red Threshold Low",         redLowThresh).getEntry();
+    redLowThreshShuffle   = tab.add("Red Threshold High",        redLowThresh).getEntry();
+    greenLowThreshShuffle = tab.add("Green Threshold Low",       greenLowThresh).getEntry();
+    greenLowThreshShuffle = tab.add("Green Threshold High",      greenLowThresh).getEntry();
+    blueLowThreshShuffle  = tab.add("Blue Threshold Low",        blueLowThresh).getEntry();
+    blueLowThreshShuffle  = tab.add("Blue Threshold High",       blueLowThresh).getEntry();
+    proxLowThreshShuffle  = tab.add("Proximity Threshold Low",   proxLowThresh).getEntry();
+    proxLowThreshShuffle  = tab.add("Proximity Threshold High",  proxLowThresh).getEntry();
+
+    tab.addNumber  ("Red",       this::getRed);
+    tab.addNumber  ("Green",     this::getGreen);
+    tab.addNumber  ("Blue",      this::getBlue);
+    tab.addNumber  ("Proximity", this::getProximity);
+    tab.addBoolean ("Target?",   this::isTarget);
   }
+
+  // TODO: Does polling shuffleboard on every method call have performance implications?
 
   public boolean isTarget() {
     return (isRedThresholdMet() && isGreenThresholdMet() && isBlueThresholdMet() &&
@@ -40,18 +78,26 @@ public class RevColorSensor {
   }
 
   public boolean isRedThresholdMet() {
+    redLowThresh = redLowThreshShuffle.getNumber(redLowThresh).intValue();
+    redHighThresh = redHighThreshShuffle.getNumber(redHighThresh).intValue();
     return (getRed() > redLowThresh && getRed() < redHighThresh);
   }
 
   public boolean isGreenThresholdMet() {
+    greenLowThresh = greenLowThreshShuffle.getNumber(greenLowThresh).intValue();
+    greenHighThresh = greenHighThreshShuffle.getNumber(greenHighThresh).intValue();
     return (getGreen() > greenLowThresh && getGreen() < greenHighThresh);
   }
 
   public boolean isBlueThresholdMet() {
+    blueLowThresh = blueLowThreshShuffle.getNumber(blueLowThresh).intValue();
+    blueHighThresh = blueHighThreshShuffle.getNumber(blueHighThresh).intValue();
     return (getBlue() > blueLowThresh && getBlue() < blueHighThresh);
   }
 
   public boolean isProximityMet() {
+    proxLowThresh = proxLowThreshShuffle.getNumber(proxLowThresh).intValue();
+    proxHighThresh = proxHighThreshShuffle.getNumber(proxHighThresh).intValue();
     return (getProximity() > proxLowThresh && getProximity() < proxHighThresh);
   }
 
